dc3d4d92929ae1125f7a2bfe31d785b06c29646f
Gabriele Maira <gabriele.maira@manifesto.co.uk>
2017-10-23 09:27:19 +0100

    D8 initial port

---
+++ src/ShorthandStoryTranslationHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\shorthand;
+
+use Drupal\content_translation\ContentTranslationHandler;
+
+/**
+ * Defines the translation handler for shorthand_story.
+ */
+class ShorthandStoryTranslationHandler extends ContentTranslationHandler {
+
+  // Override here the needed methods from ContentTranslationHandler.
+
+}
